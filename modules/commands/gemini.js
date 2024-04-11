8d8e6046f1d0e5ab95f9ec2eb39eeee57500e703
dbskie <166503823+dbskie@users.noreply.github.com>
2024-04-11 21:06:13 +0800

    Update gemini.js 4

--- modules/commands/gemini.js
+++ modules/commands/gemini.js
@@ -1,6 +1,6 @@
 
 module.exports.config = { name: "gemini", hasPermssion: 0, credits: 'Deku', // https://facebook.com/joshg101
-description: "Talk to Gemini (conversational)", usePrefix: false, commandCategory: 'chatbots', usages: 'Ai [prompt]', cooldowns: 5, }, start: async function ({ event, text, reply, react }) { const axios = require("axios"); let prompt = text.join(" "), uid = event.senderID, url; if (!prompt) return reply(Please enter a prompt.); react(''); try { const api = 'https://gemini-api.replit.app'; if (event.type == "message_reply"){ if (event.messageReply.attachments[0]?.type == "photo"){ url = encodeURIComponent(event.messageReply.attachments[0].url); const res = (await axios.get(api + "/gemini?prompt="+prompt+&url="+url+"&uid="+uid)).data;
+                         usePrefix: false, commandCategory: 'chatbots', usages: 'Ai [prompt]', cooldowns: 5, }, start: async function ({ event, text, reply, react }) { const axios = require("axios"); let prompt = text.join(" "), uid = event.senderID, url; if (!prompt) return reply(Please enter a prompt.); react(''); try { const api = 'https://gemini-api.replit.app'; if (event.type == "message_reply"){ if (event.messageReply.attachments[0]?.type == "photo"){ url = encodeURIComponent(event.messageReply.attachments[0].url); const res = (await axios.get(api + "/gemini?prompt="+prompt+&url="+url+"&uid="+uid)).data;
         return reply(res.gemini)
         } else {
           return reply('Please reply to an image.')
