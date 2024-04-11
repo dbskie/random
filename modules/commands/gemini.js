3a5ae3c4ea0756ea516fdb4e9db4c2ea444d27ed
dbskie <166503823+dbskie@users.noreply.github.com>
2024-04-11 21:24:52 +0800

    hahatdog

--- modules/commands/gemini.js
+++ modules/commands/gemini.js
@@ -1,33 +1,34 @@
-module.exports.config = { 
-    name: "gemini", 
-    hasPermssion: 0,
-    credits: 'Deku', 
-    description: "Talk to Gemini (conversational)", 
-    usePrefix: false,
-  commandCategory: 'chatbots',
-  usages: 'Ai [prompt]',
-  cooldowns: 5,
-  } 
-  start: async function ({ event, text, reply, react }) { 
-    const axios = require("axios"); 
-    let prompt = text.join(" "), 
-    uid = event.senderID, 
-    url; 
+module.exports.config = {  
+  name: "gemini",  
+  hasPermssion: 0,  
+  credits: 'Deku',  
+  description: "Talk to Gemini (conversational)",  
+  usePrefix: false,  
+  commandCategory: 'chatbots',  
+  usages: 'Ai [prompt]',  
+  cooldowns: 5
+};
 
-    if (!prompt) return reply(`Please enter a prompt.`); 
-    react(''); 
+module.exports.start = async function ({ event, text, reply, react }) {  
+  const axios = require("axios");  
+  let prompt = text.join(" ");  
+  let uid = event.senderID;  
+  let url; 
+  
+  if (!prompt) return reply("Please enter a prompt."); 
+  
+  react('✨');
+  
+  try {  
+    const api = "https://gemini-api.replit.app";   
     
-    try { 
-      const api = `https:/gemini-api.replit.app`; 
-      
-      if (event. type == "message_reply") { 
-        if (event.messageReply.attachments[0]?.type == "photo") { 
-          url = encodeURIComponent(event.messageReply.attachments[0].url); 
-          const res = await axios.get(`${api}/gemini?prompt=${prompt}`); 
-        } 
-      } 
-    } catch (error) { 
-      console.log(error); 
-    } 
-  } 
+    if (event.type == "message_reply") {  
+      if (event.messageReply.attachments[0]?.type == "photo") {  
+        url = encodeURIComponent(event.messageReply.attachments[0].url);  
+        const res = await axios.get(`${api}/gemini?prompt=${prompt}`);  
+      }  
+    }  
+  } catch (error) {  
+    console.log(error);  
+  }  
 };
